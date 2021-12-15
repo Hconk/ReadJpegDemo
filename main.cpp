2f9d57673c1c45f3f5121ceccc15bdb483687b74
Hconk <hconk0@gmail.com>
2021-12-15 11:59:50 +0800

    add tiff bug test.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <itkJPEGImageIO.h>
 #include <itkImageFileReader.h>
+#include <itkTIFFImageIO.h>
 
-int main(int, char**) {
+
+void jpg_test()
+{
     constexpr unsigned int Dimension = 2;
     using PixelType = unsigned char;
     using ImageType = itk::Image<PixelType, Dimension>;
@@ -23,5 +26,28 @@ int main(int, char**) {
     }
     
     std::cout << "ITK Read Jpeg Demo.\n";
+}
+
+bool tiff_test(const std::string &filename)
+{
+    
+  itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImgeIO(filename.c_str(), itk::ImageIOFactory::ReadMode);
+   
+  if(imageIO.IsNull) {
+    std::cout << "fail create Image io." << std::endl;
+    return false;
+  }
+  
+
+  imageIO-SetFileName(filename);
+  imageIO->ReadImageInfomation();
+  std::cout << "image pixel type: " << imageIO->GetComponentType() << std::endl;
+  std::cout << "image dim: " << imageIO->GetNumberOfDimensions() << std::endl;
+  return true;
+}
+
+int main(int, char**) {
+    jpg_test();
+    tiff_test(""D:/a/ReadJpegDemo/ReadJpegDemo/tiff_bug_1.tif"");
     return 0;
 }
